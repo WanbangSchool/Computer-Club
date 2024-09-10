594ddd157c6ac37974b82f8cd5bc1de90a0b371c
notj-code <junwonkim04@outlook.com>
2024-09-10 19:39:58 +0800

    Add stduy material

--- Problems/Goldbach's Conjecture/isPrime.cpp
+++ Problems/Goldbach's Conjecture/isPrime.cpp
@@ -1,16 +1,27 @@
 #include <bits/stdc++.h>
 using namespace std;
+/*bool prime(int n)
+{
+    if (n < 2)
+        return false;
 
-int main() {
-    bool prime(int n) {
-        if (n<2)
+    for (int x = 2; x * x <= n; x++)
+    {
+        if (n % x == 0)
             return false;
+    }
 
-        for (int x=2; x*x <= n; x++) {
-            if (n%x == 0) 
-                return false;
+    return true;
+} */
+
+int main()
+{
+    vector<bool> sieve = {0};
+    int n = 3994;
+    for (int x=2; x<=n; x++) {
+        if (sieve[x]) continue;
+        for (int u = 2*x; u<=n; u += x) {
+            sieve[u] = 1;
         }
-
-        return true;
     }
 }
