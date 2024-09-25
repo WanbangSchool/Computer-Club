553e03b772be74016ce605819b0678938731e1a0
Yeseong Lee <leeyeseong@outlook.com>
2024-09-25 18:26:06 +0800

    문제 삭제

--- Problems/ACSL/Theory Programming/TH01 answer.py
+++
@@ -1,8 +0,0 @@
-number, system = input().split()
-
-decimal = int(number,int(system))
-
-print ("BIN:",bin(decimal)[2:])
-print ("OCT:",oct(decimal)[2:])
-print ("DEC:",decimal)
-print ("HEX:",hex(decimal)[2:])
--- Problems/ACSL/Theory Programming/TH02 answer.py
+++
@@ -1,4 +0,0 @@
-hex_input = input()
-decimal = (int(hex_input,16))
-print ("비트 크기:",decimal,"비트")
-print ("바이트 크기:",decimal / 8,"바이트")
